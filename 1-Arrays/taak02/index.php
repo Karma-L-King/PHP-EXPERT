06a040473c3500f1c9144f6590bb5a832d107e58
Karma King <2114747@talnet.nl>
2020-09-07 22:13:34 +0200

    nu is 2 echt done hahaha

--- 1-Arrays/taak02/index.php
+++ 1-Arrays/taak02/index.php
@@ -30,10 +30,8 @@ $medewerkers = [
 
 /*  De medewerkers-array heeft nu meerdere arrays. */
 
-echo $medewerkers[0]['voornaam'] . " " . $medewerkers[0]['achternaam'] . " is een " . $medewerkers[0]['functie'] . "<br>" ;
-echo $medewerkers[1]['voornaam'] . " " . $medewerkers[1]['achternaam'] . " is een " . $medewerkers[1]['functie'] . "<br>" ;
-echo $medewerkers[2]['voornaam'] . " " . $medewerkers[2]['achternaam'] . " is een " . $medewerkers[2]['functie'] . "<br>" ;
-echo $medewerkers[3]['voornaam'] . " " . $medewerkers[3]['achternaam'] . " is een " . $medewerkers[3]['functie'] . "<br>" ;
-echo $medewerkers[4]['voornaam'] . " " . $medewerkers[4]['achternaam'] . " is een " . $medewerkers[4]['functie'] . "<br>" ;
 
+for ($i = 0; $i < count($medewerkers); $i++)  {
+    echo $medewerkers[$i]['voornaam'] . " " . $medewerkers[$i]['achternaam']." is een ". $medewerkers[$i]['functie'] . "<br>";
+}
 ?>
---
+++ 1-Arrays/taak03/index.php
@@ -0,0 +1,30 @@
+<?php
+$medewerkers = [
+  [
+    'voornaam' => 'Willem',
+    'achternaam' => 'van Oranje',
+    'functie'  => 'koning'
+  ],
+  [
+    'voornaam' => 'Donald',
+    'achternaam' => 'Trump',
+    'functie'  => 'president'
+  ],
+  [
+    'voornaam' => 'Homer',
+    'achternaam' => 'Simpson',
+    'functie'  => 'fabrieksarbeider'
+  ],
+  [
+    'voornaam' => 'Johan',
+    'achternaam' => 'Cruyff',
+    'functie'  => 'voetbalicoon'
+  ]
+  ,
+  [
+    'voornaam' => 'Badr',
+    'achternaam' => 'Hari',
+    'functie'  => 'Kickboxer'
+  ]
+];
+?>
