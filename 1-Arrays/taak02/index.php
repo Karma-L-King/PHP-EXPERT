943b8a2fca97a73875620716b146d961e55c8795
Karma King <2114747@talnet.nl>
2020-09-07 21:57:54 +0200

    2 is done

--- 1-Arrays/taak02/index.php
+++ 1-Arrays/taak02/index.php
@@ -30,8 +30,10 @@ $medewerkers = [
 
 /*  De medewerkers-array heeft nu meerdere arrays. */
 
+echo $medewerkers[0]['voornaam'] . " " . $medewerkers[0]['achternaam'] . " is een " . $medewerkers[0]['functie'] . "<br>" ;
+echo $medewerkers[1]['voornaam'] . " " . $medewerkers[1]['achternaam'] . " is een " . $medewerkers[1]['functie'] . "<br>" ;
+echo $medewerkers[2]['voornaam'] . " " . $medewerkers[2]['achternaam'] . " is een " . $medewerkers[2]['functie'] . "<br>" ;
+echo $medewerkers[3]['voornaam'] . " " . $medewerkers[3]['achternaam'] . " is een " . $medewerkers[3]['functie'] . "<br>" ;
+echo $medewerkers[4]['voornaam'] . " " . $medewerkers[4]['achternaam'] . " is een " . $medewerkers[4]['functie'] . "<br>" ;
 
-foreach ($medewerkers as $key => $val){  
-    echo $key."==>".$val."\n";  
-}   
 ?>
