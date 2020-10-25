424972eeae6fd82486a5d3f78716b0392d0fda53
Karma King <2114747@talnet.nl>
2020-10-25 18:31:00 +0100

    onver nieuw starten door lezen

---
+++ 3-PDO-SELECT/taak02/index copy.php
@@ -0,0 +1,12 @@
+<?php
+$hostdb = 'localhost';
+$namedb = 'locations';
+$userdb = 'root';
+$passdb = '';
+
+
+  $conn = new PDO("mysql:host=$hostdb; dbname=$namedb", $userdb, $passdb);
+ 
+  
+?>
+
--- 3-PDO-SELECT/taak02/index.php
+++ 3-PDO-SELECT/taak02/index.php
@@ -1,6 +1,17 @@
 <?php
 $hostdb = 'localhost';
-$namedb = 'locations';
+$namedb = 'toolsforever';
 $userdb = 'root';
 $passdb = '';
+
+
+$conn = new PDO("mysql:host=$hostdb; dbname=$namedb", $userdb, $passdb);
+ 
+  
+$sql = "SELECT firstname, lastname, email, id FROM users";
+$result = $conn->query($sql);
+
+  foreach($result as $row) {
+    echo "<h2>".$row['firstname']. " " .$row['lastname']. " </h2>" .' Email:'. $row['email'] . "<br>". "Rol:" . $row['id'] . '<br>';
+  }
 ?>
